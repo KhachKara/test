5c2eb528db101f4a2278037e544c0120bd71c0a2
KhachKara <khachkara@gmail.com>
2019-06-02 18:47:48 +0300

    .

---
+++ !!!.py
@@ -0,0 +1,12 @@
+class Person:
+    def __int__(self, firstname, lastname, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+pl = Person()
+pl.firstname = "Khach"
+pl.lastname = "Kara"
+pl.age = 46
+pl.nike = "Boga"
+print("{} {}".format(pl.firstname, pl.lastname))
